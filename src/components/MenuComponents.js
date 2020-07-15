bdc6a19b725e363972295734522df2f00ed31336
muneeb <muneebbutt5962@gmail.com>
2020-07-15 17:02:09 +0500

    Fetch from Server

--- src/components/MenuComponents.js
+++ src/components/MenuComponents.js
@@ -10,12 +10,14 @@ import {
 } from 'reactstrap';
 import {Link} from 'react-router-dom';
 import {Loading} from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
 
 function RenderMenuItem({dish}) {
   return (
     <Card>
       <Link to={`/menu/${dish.id}`}>
-        <CardImg width="100%" src={dish.image} alt={dish.name} />
+
+        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
         <CardImgOverlay>
           <CardTitle>{dish.name}</CardTitle>
         </CardImgOverlay>
@@ -52,22 +54,23 @@ const Menu = props => {
       </div>
     );
   } else {
-    return(
-    <div className="container">
-      <div className="row">
-        <Breadcrumb>
-          <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-          <BreadcrumbItem active>Menu</BreadcrumbItem>
-        </Breadcrumb>
-        <div className="col-12">
-          <h3>Menu</h3>
-          <hr />
+    return (
+      <div className="container">
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+            <BreadcrumbItem active>Menu</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>Menu</h3>
+            <hr />
+          </div>
+        </div>
+        <div className="row">
+          {menu}
         </div>
       </div>
-      <div className="row">
-        {menu}
-      </div>
-    </div>);
+    );
   }
 };
 
